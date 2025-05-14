5cc7eda617dd497b113b2c782ad3de3e54bd3328
jeanborech <jeanmiguelborech@gmail.com>
2025-05-14 14:19:42 -0300

    commit

---
+++ backend/src/main/java/br/com/criandoapi/projeto/DAO/IUsuario.java
@@ -0,0 +1,9 @@
+package br.com.criandoapi.projeto.DAO;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.com.criandoapi.projeto.entity.Usuario;
+
+public interface IUsuario extends CrudRepository<Usuario, Integer> {
+
+}
---
+++ backend/src/main/java/br/com/criandoapi/projeto/entity/Usuario.java
@@ -0,0 +1,63 @@
+package br.com.criandoapi.projeto.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    private String nome;
+    private String email;
+    private String senha;
+    private String telefone;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+}
